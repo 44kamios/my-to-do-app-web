3149b46d7e473e13172031bfa9438f8ec3de4ee1
k.michaluk <44kamios@gmail.com>
2023-10-26 20:47:53 +0200

    Bonus APP - make photo to gray

---
+++ bonuses/bonus19_webApp.py
@@ -0,0 +1,23 @@
+# Take input from laptop camera or photo form disk and make it gray
+
+import streamlit as st
+from PIL import Image
+
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_camera_img = img.convert('L')
+    st.image(gray_camera_img)
+else:
+    st.text("Or")
+    with st.expander("Start camera"):
+
+        camera_image = st.camera_input("Camera")
+
+    if camera_image:
+        img = Image.open(camera_image)
+        gray_camera_img = img.convert('L')
+        st.image(gray_camera_img)
